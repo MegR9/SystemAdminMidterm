1b4916b26c7410c5cd56ba583d4fdb49c1581421
MPredestin <mpredestin5782@floridapoly.edu>
2025-07-15 22:19:27 -0400

    Add files via upload

---
+++ check_agent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SERVICE=agent.service
+
+if ! systemctl is-active --quiet $SERVICE; then
+    echo "$(date): $SERVICE was down, restarting..." >> /var/log/agent_restart.log
+    systemctl restart $SERVICE
+fi
+
