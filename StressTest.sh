4be15d94a236111071eedfbafead87b3eacbb0eb
Ad3l31n3 <adomel555@gmail.com>
2025-07-16 15:18:00 -0400

    Added comments

--- StressTest.sh
+++ StressTest.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+//stresses the CPU
 stress-ng --cpu-load 50 --timeout 10s
 sleep 5
 
+//stresses the RAM
 stress-ng --vm 1 --vm-bytes 512M --timeout 10s
 sleep 5
 
+//stresses the Disk
 stress-ng --io 4 --timeout 10s
 sleep 5
 
+//stresses the Storage
 stress-ng --hdd 4 --hdd-bytes 1G --timeout 10s
 sleep 5
 
+//stresses the Network Usage
 ping -i 0.003 -f -w10 8.8.8.8
 sleep 5
