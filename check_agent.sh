9bf39b1edd388bee20d461ccb4bb67eecee0f149
MPredestin <mpredestin5782@floridapoly.edu>
2025-07-17 13:37:53 -0400

    Update check_agent.sh

--- check_agent.sh
+++ check_agent.sh
@@ -2,6 +2,7 @@
 
 SERVICE=agent.service
 
+# when agent.service is down, sends a restart message to log and resets service
 if ! systemctl is-active --quiet $SERVICE; then
     echo "$(date): $SERVICE was down, restarting..." >> /var/log/agent_restart.log
     systemctl restart $SERVICE
