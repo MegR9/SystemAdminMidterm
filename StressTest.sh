dbce0d3f9670935c9257849d4c4b1b8e474b7cc1
Ad3l31n3 <adomel555@gmail.com>
2025-07-17 15:35:08 -0400

    Update StressTest.sh
    Fixed a very minor issue where the system would take 2 five second breaks after pinging the google account

--- StressTest.sh
+++ StressTest.sh
@@ -16,6 +16,5 @@ sleep 5
 stress-ng --hdd 4 --hdd-bytes 1G --timeout 10s
 sleep 5
 
-//stresses the Network Usage
+//stresses the Network Usage (Does not have a sleep command since the system does that automatically when it loops)
 ping -i 0.003 -f -w10 8.8.8.8
-sleep 5
